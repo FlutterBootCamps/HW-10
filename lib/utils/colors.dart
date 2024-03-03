5cfcdc5bc0f0a9aafe46a80ecfec993960adda81
hmody360 <s441010207@st.uqu.edu.sa>
2024-03-03 18:46:00 +0300

    Feature: made the amiibo finder app

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const starYellow = Color(0xffF4AA01);
+const blackColor = Colors.black;
+const whiteColor = Colors.white;
+const redColor = Color(0xffE6001D);
+const greenColor = Color(0xffA5C70E);
+const greyColor = Color(0xFFA3A3A3);
+const tealColor = Color(0xff029582);
